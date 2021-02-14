0647f5765aa36de1a1ad17fc24e146ff3c1ba27b
Aditya149349 <52600886+Aditya149349@users.noreply.github.com>
2021-02-14 19:34:42 +0530

    Update 4.r

--- PartB/4.r
+++ PartB/4.r
@@ -2,7 +2,8 @@
 library("rCBA")
 data <- iris
 
-train <- data.frame(sapply(iris))
+train <- sapply(iris,as.factor)
+train <- data.frame(train,check.names = FALSE)
 txns <- as(train,"transactions")
 
 rules = rCBA::fpgrowth(txns, support=0.03, confidence=0.03, maxLength=2, consequent="Species")
